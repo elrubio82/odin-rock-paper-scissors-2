401962598454381d8323e60aeb6075d34a0411f5
Luca Mancini <167104083+elrubio82@users.noreply.github.com>
2026-05-22 16:22:26 +0100

    Add Ui to play using buttonsSubject line

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice() {
     let toReturn;
     let random = Math.random();
@@ -13,68 +12,91 @@ function getComputerChoice() {
     console.log("Computer Choice: " + toReturn);
     return toReturn;
 }
-function getHumanChoice() {
-    let input = prompt("Rock , Paper , Scissors ?");
-    console.log("Human Choice: " + input);
-    return input;
+function showMessageInParagraph(paragraphSelector, message) {
+    document.querySelector(paragraphSelector).innerText = message;
 }
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice.toLowerCase() === "rock") {
-            switch (computerChoice.toLowerCase()) {
-                case "rock":
-                    console.log("Both have chosen rock!")
-                    break;
-                case "paper":
-                    console.log("You lose! Paper beats Rock");
-                    computerScore++;
-                    break;
-                case "scissors":
-                    console.log("You win! Rock beats Scissor");
-                    humanScore++;
-                    break;
-            }
-        } else if (humanChoice.toLowerCase() === "paper") {
-            switch (computerChoice.toLowerCase()) {
-                case "rock":
-                    console.log("You win! Paper beats Rock");
-                    humanScore++;
-                    break;
-                case "paper":
-                    console.log("Both have chosen paper!")
-                    break;
-                case "scissors":
-                    console.log("You loose! Scissors beats Paper");
-                    computerScore++;
-                    break;
-            }
-        } else {
-            switch (computerChoice.toLowerCase()) {
-                case "rock":
-                    console.log("You loose! Rock beats Scissor");
-                    computerScore++;
-                    break;
-                case "paper":
-                    console.log("You win! Scissor beats paper");
-                    humanScore++;
-                    break;
-                case "scissors":
-                    console.log("Both have chosen scissors!")
-                    break;
-            }
+function playRound(humanChoice, computerChoice) {
+    if (round === 0) {
+        showMessageInParagraph(".result .total", "");
+        showMessageInParagraph(".result .partial", "");
+        showMessageInParagraph(".result .newGame", "");
+    }
+    if (humanChoice.toLowerCase() === "rock") {
+        switch (computerChoice.toLowerCase()) {
+            case "rock":
+                showMessageInParagraph(".result .message", "Both have chosen rock !")
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore)
+                break;
+            case "paper":
+                computerScore++;
+                showMessageInParagraph(".result .message", "You lose! Paper beats Rock !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+            case "scissors":
+                humanScore++;
+                showMessageInParagraph(".result .message", "You win! Rock beats Scissor !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+        }
+    } else if (humanChoice.toLowerCase() === "paper") {
+        switch (computerChoice.toLowerCase()) {
+            case "rock":
+                humanScore++;
+                showMessageInParagraph(".result .message", "You win! Paper beats Rock !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+            case "paper":
+                showMessageInParagraph(".result .message", "Both have chosen paper !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore)
+                break;
+            case "scissors":
+                computerScore++;
+                showMessageInParagraph(".result .message", "You loose ! Scissors beats Paper !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+        }
+    } else {
+        switch (computerChoice.toLowerCase()) {
+            case "rock":
+                computerScore++;
+                showMessageInParagraph(".result .message", "You loose! Rock beats Scissor !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+            case "paper":
+                humanScore++;
+                showMessageInParagraph(".result .message", "You win! Scissor beats paper !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore);
+                break;
+            case "scissors":
+                showMessageInParagraph(".result .message", "Both have chosen scissors !");
+                showMessageInParagraph(".result .partial", "Partial Result =" + " You : " + humanScore + " , Computer : " + computerScore)
+                break;
         }
     }
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log("Human win " + humanScore + " : " + computerScore);
-    } else if (humanScore === computerScore) {
-        console.log("No one win " + computerScore + " : " + humanScore);
-    } else {
-        console.log("Computer win " + computerScore + " : " + humanScore);
+    round++;
+    if (humanScore === 5 || computerScore == 5) {
+        if (humanScore > computerScore) {
+            showMessageInParagraph(".result .total", "Human win " + humanScore + " : " + computerScore + " !");
+        } else if (humanScore === computerScore) {
+            showMessageInParagraph(".result .total", "No one win " + computerScore + " : " + humanScore);
+        } else {
+            showMessageInParagraph(".result .total", "Computer win " + computerScore + " : " + humanScore + " !");
+        }
+        showMessageInParagraph(".result .newGame", "Make your choice if you would like to play a new game ...");
+        round = 0;
+        humanScore = 0;
+        computerScore = 0;
     }
 }
-playGame();
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+document.querySelectorAll(".userChoice button").forEach(
+    function (button) {
+        button.addEventListener("click",
+            function (event) {
+                playRound(event.target.id, getComputerChoice());
+            });
+    });
+
